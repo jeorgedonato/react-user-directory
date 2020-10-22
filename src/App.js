a16dbbfa1e3877d9a150b206e84dc1d1e7bf4444
Jeorge Donato <jeorgekhenr@gmail.com>
2020-10-22 16:00:55 -0700

    Added wrapper

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Wrapper from './components/Wrapper'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Router>
+      <div>
+        <Wrapper>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/search" component={Search} />
+        </Wrapper>
+      </div>
+    </Router>
   );
 }
 
