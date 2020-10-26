c4cff3b400dc0d52af75ec32f1dc6f401d85870e
Jeorge Donato <jeorgekhenr@gmail.com>
2020-10-25 21:28:52 -0700

    Updates

---
+++ precache-manifest.100bcba41931354a0e9519788cd6b169.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c124884c2a0edc6e2428b736e7dec471",
+    "url": "/react-user-directory/index.html"
+  },
+  {
+    "revision": "d143cbd7776fa0ba3d83",
+    "url": "/react-user-directory/static/css/main.2aa8617e.chunk.css"
+  },
+  {
+    "revision": "908a4abc29dfa34b72d8",
+    "url": "/react-user-directory/static/js/2.bd171f05.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/react-user-directory/static/js/2.bd171f05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d143cbd7776fa0ba3d83",
+    "url": "/react-user-directory/static/js/main.6d3bd61e.chunk.js"
+  },
+  {
+    "revision": "4a6e7c5fdd4c6fee0720",
+    "url": "/react-user-directory/static/js/runtime-main.e7548267.js"
+  }
+]);
